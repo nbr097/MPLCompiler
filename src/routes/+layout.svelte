7d2e7e091ff5767f1fdba586187fd64d77291030
nbr097 <nicho.brown2@gmail.com>
2025-09-06 00:14:35 +1000

    Update +layout.svelte
    removed top text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,3 @@
-export const ssr = false;       // render pages on the client
-export const prerender = true;  // emit static HTML at build time
 <script lang="ts">
   import '../app.css'; // Tailwind entry
 </script>

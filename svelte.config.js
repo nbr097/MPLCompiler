b1c3ed55c8a2183fe43ee222be975fbe2ee492f9
nbr097 <nicho.brown2@gmail.com>
2025-09-05 23:57:08 +1000

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-// svelte.config.js
 import adapter from '@sveltejs/adapter-cloudflare';
 
 const config = {
   kit: {
     adapter: adapter()
   }
-  // no preprocess needed for Tailwind/PostCSS
 };
 
 export default config;
